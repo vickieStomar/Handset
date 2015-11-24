3046501eedea69859b0f2f09b82f0c1f84e4499c
VT <vtr@gmail.com>
2015-11-24 13:09:26 +0530

    Functionality Added.

---
+++ in/handset/Handset.java
@@ -0,0 +1,10 @@
+package in.handset;
+
+import in.shared.Shared;
+
+public class Handset{
+	public static void main(String arg[]){
+		Shared shared = new Shared("Handset");		
+		System.out.println(shared);
+	}
+}
